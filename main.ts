550a1063887a686fb18f95ac5858be6abb0582b2
alimurad4555 <alimurad4555gmail.com>
2024-05-07 02:09:02 -0700

    i defin increment_decrement

---
+++ main.ts
@@ -0,0 +1,38 @@
+// 1 increment 
+let x =200;
+// pre-increment 
+//++x; first update value daga 
+
+//console.log(x); //--> 200 x ki jwo value thi isna wohe value de
+//console.log(++x); // -->201 first update kar ka hamna per-increment lagya two 201 naswer aya
+//console.log(++x); // -->202 asa he jitne dafa ham per-increment lagya ga two 1 jama kar 
+//console.log(++x); // -->203 value data rahya ga
+
+// post-increment
+//x++; secend update value daga
+
+let num2 =x++;
+//console.log(num2); // --> 200  
+//console.log(x++); // --> 201 secend update value
+//console.log(x++); // --> 202 
+//console.log(x++); // --> 203
+
+// 2 Decrement same increment ki trha bs faraq aik cheez ka increment ham jama ++ karta h our decrement me ham maynas -- karta h
+//per-decrement
+//--y
+
+let y =10;
+//--y; //per-decrement
+//console.log(--y); // --> 9 first update value
+//console.log(--y); // --> 8
+//console.log(--y); // --> 7
+
+//y-- //post-decrement 
+let num1 =y;
+//y--; //post-decrement
+//console.log(y--); // --> 10 
+//console.log(y--); // --> 9 secend update value
+//console.log(y--); // --> 8
+
+
+
